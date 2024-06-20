64a2869038f0497e9905ebebb918bf10c3a0f69a
Vladislav-Pavlenko <151663067+Vladislav-Pavlenko@users.noreply.github.com>
2024-06-20 10:07:19 +0300

    Correct logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState, useEffect } from "react";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feadback/Feadback";
 import Option from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
 export default function App() {
   const [rev, setRev] = useState(() => {
     const reviewsObj = JSON.parse(localStorage.getItem("reviews"));
-    if (reviewsObj.length === 0) {
+    if (!reviewsObj) {
       return { good: 0, neutral: 0, bad: 0 };
     } else {
       return {
@@ -62,7 +63,7 @@ export default function App() {
           positive={Math.round((rev.good / totalFeedback) * 100)}
         />
       ) : (
-        <p>No feedback yet</p>
+        <Notification />
       )}
     </>
   );
